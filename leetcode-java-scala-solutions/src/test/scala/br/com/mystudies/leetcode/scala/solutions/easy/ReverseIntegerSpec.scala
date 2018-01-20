a9819c6df033c37183cd8eed929dea8f93ac6e57
Robson Duarte <robson.o.d@gmail.com>
2018-01-20 16:39:45 -0200

    rename variable

--- leetcode-java-scala-solutions/src/test/scala/br/com/mystudies/leetcode/scala/solutions/easy/ReverseIntegerSpec.scala
+++ leetcode-java-scala-solutions/src/test/scala/br/com/mystudies/leetcode/scala/solutions/easy/ReverseIntegerSpec.scala
@@ -1,13 +1,13 @@
 package br.com.mystudies.leetcode.scala.solutions.easy
 
 class ReverseIntegerSpec extends UnitSpec{
-	
-  val reverseInt =  new ReverseInteger
 
-  it should "reverse the Int" in {
-    reverseInt.reverse(123) should be(321)
-    reverseInt.reverse(120) should be(21)
-    reverseInt.reverse(-123) should be(-321)
-    reverseInt.reverse(1534236469) should be(0)
+  val reverseInteger =  new ReverseInteger
+
+  it should "reverse the Integer" in {
+    reverseInteger reverse 123 should be(321)
+    reverseInteger reverse 120 should be(21)
+    reverseInteger reverse -123 should be(-321)
+    reverseInteger reverse 1534236469 should be(0)
   }
 }
