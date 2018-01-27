23476bbe480be52d4825c0b86dba04404e2b7574
Robson Duarte <robson.o.d@gmail.com>
2018-01-27 13:15:37 -0200

    solution in java to problem two sum

---
+++ leetcode-java-scala-solutions/src/test/java/br/com/mystudies/leetcode/java/solutions/easy/TwoSumTest.java
@@ -0,0 +1,28 @@
+package br.com.mystudies.leetcode.java.solutions.easy;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+public class TwoSumTest {
+
+
+	private TwoSum twoSum = new TwoSum();
+
+
+	@Test
+	public void test1() {
+		assertArrayEquals(new int[] {0,1}, twoSum.twoSum(new int[]{2, 7, 11, 15}, 9));
+		assertArrayEquals(new int[] {2,3}, twoSum.twoSum(new int[]{2, 7, 11, 15}, 26));
+		assertArrayEquals(new int[] {1,3}, twoSum.twoSum(new int[]{4, 6, 8, 10}, 16));
+		assertArrayEquals(new int[] {2,4}, twoSum.twoSum(new int[]{4, 6, 8, 10, 11}, 19));
+	}
+
+
+	@Test(expected=IllegalArgumentException.class)
+	public void test2() {
+		assertArrayEquals(new int[] {0,1}, twoSum.twoSum(new int[]{2, 7, 11, 15}, 15));
+	}
+
+
+}
