de0e5d5142e80df2a200ecc95b02fb356f6bd961
Robson Duarte <robson.o.d@gmail.com>
2018-02-03 13:36:07 -0200

    starting the solution o problem roman to integer in java

---
+++ leetcode-java-scala-solutions/src/main/java/br/com/mystudies/leetcode/java/solutions/easy/RomanToInteger.java
@@ -0,0 +1,10 @@
+package br.com.mystudies.leetcode.java.solutions.easy;
+
+public class RomanToInteger {
+
+	public int romanToInt(String s) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ leetcode-java-scala-solutions/src/test/java/br/com/mystudies/leetcode/java/solutions/easy/RomanToIntegerTest.java
@@ -0,0 +1,17 @@
+package br.com.mystudies.leetcode.java.solutions.easy;
+
+import org.junit.Test;
+
+public class RomanToIntegerTest {
+
+
+	
+	private RomanToInteger romanToInteger;
+
+	@Test
+	public void test() {
+		int x = romanToInteger.romanToInt("I");
+		
+	}
+
+}
